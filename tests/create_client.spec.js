6d6c0a1c47d3b520860f748d297fc5c492097a8f
Ya903040 <asirov560@gmail.com>
2023-03-25 13:03:11 +0600

    trying to do something

--- tests/create_client.spec.js
+++ tests/create_client.spec.js
@@ -8,7 +8,16 @@ test('create user ', async ({page}) => {
     await page.locator('.auth-btn').click()
 
     await page.locator('.clients-add-user-dialog').click()
-    await page.locator('#mat-input-1').fill('Yasirov')
-    await page.locator('#mat-input-2').fill('Suleiman')
+    await page.locator('#mat-input-1').fill('Yasirov6')
+    await page.locator('#mat-input-2').fill('Suleiman6')
+    await page.locator('#mat-input-3').fill('Tvumarovich6')
+    await page.locator('#mat-radio-2').click()
+    await page.locator('#mat-input-4').fill('yasirovsuleiman6@gmail.com')
+    await page.locator('#mat-input-5').fill('999999916')
 
+
+
+    await page.locator('#mat-input-6').fill('03.03.2004')
+    await page.locator("[name=save]").click()
+    await page.waitForTimeout(500)
 });
